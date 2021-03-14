fa38f5ae5a73ddb2ea166b8f4dd585828415603a
Bradley Knutson <bradley@bknutson.com>
2021-03-14 12:33:38 -0600

    Remove MetadataKeys

--- build/controllers/decorators/Methods.js
+++ build/controllers/decorators/Methods.js
@@ -4,8 +4,8 @@ exports.Methods = void 0;
 var Methods;
 (function (Methods) {
     Methods["get"] = "get";
-    Methods["post"] = "post";
     Methods["patch"] = "patch";
-    Methods["del"] = "delete";
+    Methods["post"] = "post";
     Methods["put"] = "put";
+    Methods["del"] = "delete";
 })(Methods = exports.Methods || (exports.Methods = {}));
